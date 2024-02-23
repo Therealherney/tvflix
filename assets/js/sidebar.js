097956b60f90fb9e004230e272a208f53f50e95d
Therealherney <119009952+Therealherney@users.noreply.github.com>
2024-02-23 12:28:55 +0000

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -41,7 +41,7 @@ export function sidebar() {
     <div class="sidebar-footer">
       <p class="copyright">
         Copyright 2023
-        <a href="https://naufalfiqri.netlify.app" target="_blank">HERNEYHORLAR</a>
+        <a href="https://herneyhorlar.vercel.app" target="_blank">HERNEYHORLAR</a>
       </p>
       <img src="./assets/images/tmdb-logo.png" width="130" height="17" alt="the movie database logo" />
     </div>
